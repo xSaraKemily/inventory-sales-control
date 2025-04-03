9829a28a1fe60c07aeab1c4d8b76bf72016200fc
saraKemily <sarakemili@hotmail.com>
2025-04-03 13:54:12 -0300

    refactor: renamed

--- app/Events/SaleCreated.php
+++ app/Events/SaleCreated.php
@@ -7,7 +7,7 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class SaleCompleted
+class SaleCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
